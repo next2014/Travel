83959d70f916d575e305eddb9246c4e0d8d65159
next2014 <83329265@qq.com>
2020-03-19 01:58:10 +0800

    0319

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,55 +7,47 @@
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩主题
     </div>
-    <router-link to='/city'>
-      <div class="header-right">
-        {{this.city}}
-        <span class="iconfont arrow-icon">&#xe64a;</span>
-      </div>
-    </router-link>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
 export default {
-  name: 'HomeHeader',
-  computed: {
-    ...mapState(['city'])
-  }
+  name: 'HomeHeader'
 }
 </script>
 
 <style lang="stylus" scoped>
   @import '~styles/varibles.styl'
   .header
-    display: flex
-    line-height: $headerHeight
-    background: $bgColor
-    color: #fff
+    display flex
+    line-height $headerHeight
+    background $bgColor
+    color #FFFFFF
     .header-left
-      width: .64rem
-      float: left
+      width .64rem
+      float left
       .back-icon
-        text-align: center
-        font-size: .4rem
+        text-align center
+        font-size .4rem
     .header-input
-      flex: 1
-      height: .64rem
-      line-height: .64rem
-      margin-top: .12rem
-      margin-left: .2rem
-      padding-left: .2rem
-      background: #fff
-      border-radius: .1rem
-      color: #ccc
+      flex 1
+      height .64rem
+      line-height .64rem
+      margin-top .12rem
+      margin-left .2rem
+      padding-left .2rem
+      background #FFFFFF
+      border-radius .1rem
+      color #CCCCCC
     .header-right
-      min-width: 1.04rem
-      padding: 0 .1rem
-      float: right
-      text-align: center
-      color: #fff
+      width 1.24rem
+      float right
+      text-align center
       .arrow-icon
-        margin-left: -.04rem
-        font-size: .24rem
+        margin-left -.04rem
+        font-size .24rem
 </style>
